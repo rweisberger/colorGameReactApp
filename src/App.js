e5b3e5779ef6c9ccf02dbfa1ac50c90d86659543
Rachel Weisberger <rachel.weisberger@gmail.com>
2022-09-01 22:55:24 -0400

    updating functionality, problem solving color change and state updates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {useState} from "react";
-import GridBox from './component/grid';
-
 
 function App() {
-  const [boxNum, setBoxNum] = useState(0);
-  let boxes= [1,2,3,4,5,6,7,8,9];
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState(0);
+
+  const [boxes, setBoxes] = useState([
+                              {number: 1, isActive: false },
+                              {number: 2, isActive: false },
+                              {number: 3, isActive: false },
+                              {number: 4, isActive: false },
+                              {number: 5, isActive: false },
+                              {number: 6, isActive: false },
+                              {number: 7, isActive: false },
+                              {number: 8, isActive: false },
+                              {number: 9, isActive: false }
+                            ]);
+
 
   const changeColor = () => {
-    setBoxNum(value);
-   }
+    console.log(value);
+    let selectedBox = boxes.find((box)=> box.number == value )
+    selectedBox.isActive = !selectedBox.isActive;
+    console.log(selectedBox);
+    console.log(boxes)
+    setBoxes([...boxes]);
+  }
 
   return (
     <div className="container">
       <h1>Color Game</h1>
       <div className="wrapper">
         <div>
-          <input type="number" placeholder="Enter grid number" value= {value} onChange={(e)=>setValue(e.target.value)}/><br/>
+          <input type="number" placeholder="Enter grid number" value={value} onChange={(e)=>setValue(e.target.value)}/><br/>
           <button type="button" className="btn btn-outline-success" onClick={changeColor}>Click</button>
         </div>
         <div className='game-board'> 
-        {boxes.map((box, i) => (
-          <GridBox key={ i } id={i} box={ box } selectedBox= {boxNum} function={changeColor}/>
-        ))}
+          {boxes.map((element, i) => (
+            <div key={i} className= {element.isActive ? 'grid-box2' : 'grid-box1'}>{element.number}</div>
+          ))}
         </div>
       </div> 
     </div>
@@ -34,4 +48,3 @@ function App() {
 export default App;
 
 
-        // {/* {boxes.map((box,i) => <div className={isActive ? "grid-box1" : "grid-box2"} key={i} id={boxes[i]}>{boxes[i]}</div>)} */}
